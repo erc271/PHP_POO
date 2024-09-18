fb2a71d555b90ce8041592c8ddd3679da1aa0c4c
Ayoub <ayoubsolicode07@gmail.com>
2024-09-18 10:19:47 +0100

    tuto1

---
+++ tuto1.php
@@ -0,0 +1,19 @@
+<?php
+class Voiture {
+    public $marque;
+    public $modele;
+    public $couleur;
+
+    public function demarrer() {
+        echo "La ".$this->modele." voiture démarre.";
+    }
+}
+
+// Création d'un objet Voiture
+$maVoiture = new Voiture();
+$maVoiture->marque = "Renault";
+$maVoiture->modele = "Clio";
+$maVoiture->couleur = "Bleu";
+
+$maVoiture->demarrer();
+?>
