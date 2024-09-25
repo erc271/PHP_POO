7742bd2bd7de980171b5695b277defc5a352ae2d
Ayoub <ayoubsolicode07@gmail.com>
2024-09-25 16:51:39 +0100

    local dns

--- index_deployement.php
+++ index_deployement.php
@@ -1,4 +1,6 @@
 <?php
 echo "<h1>Hello World</h1>";
-echo "<p>Link for the tutorial that I followed: <a href='https://www.youtube.com/watch?v=3EAj9tsXLFU'>https://www.youtube.com/watch?v=3EAj9tsXLFU</a></p>";
+echo "<p>Link for the tutorial that I followed: <a href='https://www.youtube.com/watch?v=3EAj9tsXLFU'>Youtube</a></p>";
+echo "\n<p>ChatGpt Link for DNS that I followed: <a href='https://chatgpt.com/share/66f4051d-126c-8010-a6ea-13957a2ce98c'>ChatGpt Link\n";
+echo"\n<a href='http://192.168.2.191/web/index_deployement.php'>DNSServer";
 ?>
